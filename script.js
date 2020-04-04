16ac83149494745496f8dfade0ad8b8b2dd49fbf
Evan Dobalian <evandobalian@gmail.com>
2020-04-04 13:25:29 -0700

    Script testing

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
-var query = '';
-var queryURL = `https://api.nytimes.com/svc/search/v2/articlesearch.json?q=${query}&api-key=EGrEM8JhMsFYGbORWTHBudSmGTGPAhGh`;
-
+$(document).ready(function () {
+    var query = '';
+    var queryURL = `https://api.nytimes.com/svc/search/v2/articlesearch.json?q=${query}&api-key=EGrEM8JhMsFYGbORWTHBudSmGTGPAhGh`;
+$('.submit').on('click', function () {
+    event.preventDefault();
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+      }).then(function (response) {
+        console.log(response);
+      });
+});
+});
